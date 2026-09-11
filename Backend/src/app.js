7e0a7ff1d270c97267f593e3f80d587d0afca752
Aniket04-05 <aniketkumar04055@gmail.com>
2026-09-12 00:48:54 +0530

    modify

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 // 1. SECURITY: Dynamic CORS for Production
 app.use(cors({
     // Uses the deployed frontend URL if available, otherwise defaults to local Vite port
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
+    origin: process.env.FRONTEND_URL, 
     credentials: true
 }));
 
