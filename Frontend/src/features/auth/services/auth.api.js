3327f537229c4c888a82c808e1b89cf900463c38
Aniket04-05 <aniketkumar04055@gmail.com>
2026-09-12 11:18:53 +0530

    changes in environment variable

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // 1. SCALABILITY: Use Vite's environment variables with a local fallback
 const api = axios.create({
-    baseURL: import.meta.env.BACKEND_URL || "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
     withCredentials: true
 });
 
